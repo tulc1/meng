0c4727ec05d993e95a30057a0ee2d8726f656e7b
tulc1 <2716072841@qq.com>
2024-12-29 15:33:22 +0800

    meng

--- src/run/run.sh
+++ src/run/run.sh
@@ -75,7 +75,121 @@
 
 
 # ========= book ================
-python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 0.90 --temp 10 --dataset music
-# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 0.90 --temp 10 --dataset music
-# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 0.90 --temp 10 --dataset music
+# 0.0866	0.0916
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+# 0.0832	0.0943
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 2.00 --temp 10 --dataset book
+# 0.0778	0.0860
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 3.00 --temp 10 --dataset book
+# 0.0931	0.1056
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 4.00 --temp 10 --dataset book
+# 0.0903	0.1027
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 5.00 --temp 10 --dataset book
+
+# 0.0914	0.1164
+# python main.py --L 3 --H 3 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+# 0.1085	0.1260
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+# 0.1091	0.1105
+# python main.py --L 5 --H 5 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+# 0.0808	0.1111
+# python main.py --L 6 --H 6 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+# 0.0680	0.0680
+# python main.py --L 7 --H 7 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+
+# 0.0835	0.0935
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 1e-3 --p 1.00 --temp 10 --dataset book
+# 0.0874	0.1123
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 1e-4 --p 1.00 --temp 10 --dataset book
+# 0.0910	0.0974
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-5 --p 1.00 --temp 10 --dataset book
+# 0.0940	0.1065
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 1e-5 --p 1.00 --temp 10 --dataset book
+# 0.0879	0.1015
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-6 --p 1.00 --temp 10 --dataset book
+# 0.1086	0.1115
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 1e-6 --p 1.00 --temp 10 --dataset book
+
+# 0.0849	0.1049
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 1 --dataset book
+# 0.0857	0.1088
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 5 --dataset book
+# 0.0866	0.0916
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 10 --dataset book
+# 0.0843	0.1074
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 20 --dataset book
+# 0.0967	0.1109
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 30 --dataset book
+# 0.0943	0.0985
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 40 --dataset book
+# 0.0986	0.1036
+# python main.py --L 2 --H 2 --kge_interval 3 --ssl_weight 5e-4 --p 1.00 --temp 50 --dataset book
+
+#0.0920	0.1120
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 5e-4 --p 4.00 --temp 10 --dataset book
+# 0.1060	0.1427
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 5e-4 --p 4.00 --temp 30 --dataset book
+# 0.0903	0.0978
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-5 --p 4.00 --temp 30 --dataset book
+# 0.0985	0.1145
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 5e-5 --p 4.00 --temp 30 --dataset book
+# 0.1070	0.1470
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 30 --dataset book
+# 0.1017	0.1042
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-3 --p 4.00 --temp 30 --dataset book
+
+# 0.0960	0.1310
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 1.00 --temp 30 --dataset book
+# 0.1103	0.1410
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 2.00 --temp 30 --dataset book
+# 0.1134	0.1241
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 3.00 --temp 30 --dataset book
+# 0.1160	0.1260
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 30 --dataset book
+# 0.0942	0.1217
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 5.00 --temp 30 --dataset book
+# 0.0920	0.1070
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 6.00 --temp 30 --dataset book
+# 0.0985	0.1370
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 7.00 --temp 30 --dataset book
+# 0.1335	0.1349
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 8.00 --temp 30 --dataset book
+
+# 0.1033	0.1205
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 1 --dataset book
+# 0.1085	0.1110
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 5 --dataset book
+# 0.1010	0.1170
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 10 --dataset book
+# 0.1110	0.1110
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 15 --dataset book
+# 0.0793	0.0958
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 20 --dataset book
+# 0.0960	0.0960
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 25 --dataset book
+# 0.1160	0.1260
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 30 --dataset book
+# 0.1195	0.1334
+# 0.0867	0.1130
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 35 --dataset book
+# 0.0820	0.1120
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 40 --dataset book
+# 0.1220	0.1370
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 45 --dataset book
+# 0.0984	0.1384
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 50 --dataset book
+# 0.0835	0.1410
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 55 --dataset book
+# 0.0910	0.1320
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 60 --dataset book
+# 0.0774	0.1158
+python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 65 --dataset book
+# 0.0735	0.1292
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 70 --dataset book
+
+# 0.1120	0.1270
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 30 --dataset book
+# 0.0935	0.1334
+# python main.py --L 4 --H 4 --kge_interval 3 --ssl_weight 1e-4 --p 4.00 --temp 30 --dataset book
+
 
